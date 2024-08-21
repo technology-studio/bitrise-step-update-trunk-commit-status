ce141a44bef9caf2277b78940c3ab82202292e81
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2024-08-21 21:04:59 +0200

    fix: extract release candidate logic to a separate step (#4)
    * feat: add delivered release candidate branch creation and updating
    
    * fix: remove marking of release candidate tag and branch
    
    * fix: update context value and remove process.exit
    
    * fix: remove duplicated slash
    
    * fix: remove redundant info from context

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 import ky from 'ky';
 import { execSync } from 'child_process';
 
-import {
-  getAndroidVersion,
-  getIOSVersion,
-} from './getAppVersions.js';
-
 const ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 const COMMIT_SHA = process.env.COMMIT_SHA;
 const REPO = process.env.REPO_SLUG;
@@ -13,10 +8,8 @@ const OWNER = process.env.REPO_OWNER;
 const BITRISE_STATUS = process.env.BITRISE_BUILD_STATUS;
 const IS_PENDING = process.env.IS_PENDING;
 const BUILD_URL = process.env.BITRISE_BUILD_URL;
-const BUILD_NUMBER = process.env.BITRISE_BUILD_NUMBER;
 const IS_BUILD_FAILED = process.env.IS_BUILD_FAILED;
 const CONTEXT = process.env.CONTEXT;
-const PLATFORM = process.env.APP_PLATFORM;
 
 const api = ky.create({
   prefixUrl: 'https://api.github.com',
@@ -63,34 +56,5 @@ const api = ky.create({
   } catch (error) {
     console.error("Failed to update status");
     console.error(error);
-  }
-
-  if (state === "success" && IS_PENDING === "no") {
-    const appVersion = PLATFORM === "ios" ? getIOSVersion() : getAndroidVersion();
-    if (appVersion === null) {
-      console.log("No app version found, skipping");
-      return;
-    }
-    const releaseCandidateTagName = `release-candidate/${PLATFORM}/${appVersion}-${BUILD_NUMBER}`;
-    console.log(`Adding "release-candidate" tag to ${COMMIT_SHA}...`);
-    try {
-      const tagResponse = await api.post(`repos/${OWNER}/${REPO}/git/tags`, {
-        json: {
-          tag: releaseCandidateTagName,
-          message: "",
-          object: COMMIT_SHA,
-          type: "commit",
-        },
-      }).json();
-      const tagSha = tagResponse.sha;
-      await api.post(`repos/${OWNER}/${REPO}/git/refs`, {
-        json: {
-          ref: `refs/tags/${releaseCandidateTagName}`,
-          sha: tagSha,
-        },
-      });
-    } catch (error) {
-      console.error("Failed to create or add release-candidate tag");
-    }
   }
 })();
